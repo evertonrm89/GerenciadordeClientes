778c02944b95a275ed27d1604cfaebffb9801fe7
Everton R Machado <evertonrm89@gmail.com>
2015-05-13 18:53:40 -0300

    Nova tela de exibir Cliente, arrumando o cadastro e editar cliente

---
+++ app/build/intermediates/classes/debug/evertonrmachado/gclientes/fragment/ClienteCadAltBtnFrag$$ViewInjector.java
@@ -0,0 +1,21 @@
+// Generated code from Butter Knife. Do not modify!
+package evertonrmachado.gclientes.fragment;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+import butterknife.ButterKnife.Injector;
+
+public class ClienteCadAltBtnFrag$$ViewInjector<T extends evertonrmachado.gclientes.fragment.ClienteCadAltBtnFrag> implements Injector<T> {
+  @Override public void inject(final Finder finder, final T target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131296351, "field 'btnCancelar'");
+    target.btnCancelar = finder.castView(view, 2131296351, "field 'btnCancelar'");
+    view = finder.findRequiredView(source, 2131296352, "field 'btnSalvar'");
+    target.btnSalvar = finder.castView(view, 2131296352, "field 'btnSalvar'");
+  }
+
+  @Override public void reset(T target) {
+    target.btnCancelar = null;
+    target.btnSalvar = null;
+  }
+}
---
+++ app/build/intermediates/classes/debug/evertonrmachado/gclientes/fragment/ClienteCadAltFrag$$ViewInjector.java
@@ -0,0 +1,45 @@
+// Generated code from Butter Knife. Do not modify!
+package evertonrmachado.gclientes.fragment;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+import butterknife.ButterKnife.Injector;
+
+public class ClienteCadAltFrag$$ViewInjector<T extends evertonrmachado.gclientes.fragment.ClienteCadAltFrag> implements Injector<T> {
+  @Override public void inject(final Finder finder, final T target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131296343, "field 'txtNome'");
+    target.txtNome = finder.castView(view, 2131296343, "field 'txtNome'");
+    view = finder.findRequiredView(source, 2131296344, "field 'txtTelefone'");
+    target.txtTelefone = finder.castView(view, 2131296344, "field 'txtTelefone'");
+    view = finder.findRequiredView(source, 2131296345, "field 'txtCelular'");
+    target.txtCelular = finder.castView(view, 2131296345, "field 'txtCelular'");
+    view = finder.findRequiredView(source, 2131296346, "field 'txtEmail'");
+    target.txtEmail = finder.castView(view, 2131296346, "field 'txtEmail'");
+    view = finder.findRequiredView(source, 2131296347, "field 'txtEndereco'");
+    target.txtEndereco = finder.castView(view, 2131296347, "field 'txtEndereco'");
+    view = finder.findRequiredView(source, 2131296348, "field 'txtCidade'");
+    target.txtCidade = finder.castView(view, 2131296348, "field 'txtCidade'");
+    view = finder.findRequiredView(source, 2131296349, "field 'txtEstado'");
+    target.txtEstado = finder.castView(view, 2131296349, "field 'txtEstado'");
+    view = finder.findRequiredView(source, 2131296350, "field 'txtObservacao'");
+    target.txtObservacao = finder.castView(view, 2131296350, "field 'txtObservacao'");
+    view = finder.findRequiredView(source, 2131296342, "field 'chkStatus'");
+    target.chkStatus = finder.castView(view, 2131296342, "field 'chkStatus'");
+    view = finder.findRequiredView(source, 2131296341, "field 'chkFavorito'");
+    target.chkFavorito = finder.castView(view, 2131296341, "field 'chkFavorito'");
+  }
+
+  @Override public void reset(T target) {
+    target.txtNome = null;
+    target.txtTelefone = null;
+    target.txtCelular = null;
+    target.txtEmail = null;
+    target.txtEndereco = null;
+    target.txtCidade = null;
+    target.txtEstado = null;
+    target.txtObservacao = null;
+    target.chkStatus = null;
+    target.chkFavorito = null;
+  }
+}
